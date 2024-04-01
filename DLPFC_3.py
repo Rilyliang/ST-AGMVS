b0184e5eeecaa12d1934e8f7e1e4dfadf49b37cf
Rilyliang <55230161+Rilyliang@users.noreply.github.com>
2024-04-01 10:31:50 +0800

    Update DLPFC_3.py

--- DLPFC_3.py
+++ DLPFC_3.py
@@ -23,11 +23,10 @@ warnings.filterwarnings('ignore')
 
 ari_avg = 0.0
 nmi_avg = 0.0
-# data_list = ['151507', '151508', '151509', '151510', '151669', '151670', '151671', '151672', '151674', '151675', '151676']
-data_list = ['151507']
+data_list = ['151507', '151508', '151509', '151510', '151669', '151670', '151671', '151672', '151674', '151675', '151676']
 for i in data_list:
     fix_seed(42)
-    data_path = 'D:/scientific study/data/82_spatialLIBD_LIBD human dorsolateral pre-frontal cortex (DLPFC) spatial transcriptomics data generated with the 10x Genomics Visium platform/'
+    data_path = '............/'
     section_name = i
     print(section_name)
     adata = read_10X_DLPFC(data_path, section_name)
@@ -70,40 +69,6 @@ for i in data_list:
 
     ari_avg = ari_avg + ARI
     nmi_avg = nmi_avg + NMI
-
-    savepath = 'D:/scientific study/paper3/ST-GCAA/' + section_name + '/'
-    try:
-        os.mkdir(savepath)
-        print(f"文件夹创建成功")
-    except FileExistsError:
-        print(f"文件夹已经存在")
-    except Exception as e:
-        print(f"无法创建文件夹': {e}")
-
-    adata.write(savepath + 'ST-GCAA_sample_data.h5ad')
-
-    with open(savepath + "ari_and_nmi.txt", "w") as file:
-        file.write("ARI: {}\n".format(ARI))
-        file.write("NMI: {}\n".format(NMI))
-        file.write("ARI_refined: {}\n".format(ARI_refined))
-        file.write("NMI_refined: {}\n".format(NMI_refined))
-
-    title = 'ST-GCAA: ARI={:.3f}, NMI={:.3f}'.format(adata.uns['ARI_refine'], adata.uns['NMI_refine'])
-    sc.pl.spatial(adata, img_key="hires", color=['domain'], title=title, show=False)
-    plt.savefig(savepath + 'ST-GCAA.tif', bbox_inches='tight', dpi=300)
-
-    sc.pp.neighbors(adata, use_rep='emb')
-    sc.tl.umap(adata)
-    plt.rcParams["figure.figsize"] = (3, 3)
-    sc.tl.paga(adata, groups='domain')
-    sc.pl.paga_compare(adata, legend_fontsize=10, frameon=False, size=20, title=title, legend_fontoutline=2,
-                       show=False)
-    plt.savefig(savepath + 'ST-GCAA_PAGA_domain.tif', bbox_inches='tight', dpi=300)
-
-    sc.tl.paga(adata, groups='ground_truth')
-    sc.pl.paga_compare(adata, legend_fontsize=10, frameon=False, size=20, title=title, legend_fontoutline=2,
-                       show=False)
-    plt.savefig(savepath + 'ST-GCAA_PAGA_ground_truth.tif', bbox_inches='tight', dpi=300)
 
 print("ARI_AVG: ", ari_avg/12.0)
 print("NMI_AVG: ", nmi_avg/12.0)
